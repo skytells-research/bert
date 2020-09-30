350c96dc1c376186ce53c68e4dcfe196ef8416b4
Skytells, Inc <support@skytells.net>
2020-09-30 16:20:12 +0200

    Deployment Ready

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from flask import request
 import torch
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 
+# name = "twmkn9/albert-base-v2-squad2"
+# name = "distilbert-base-uncased-distilled-squad"
 name = "mrm8488/bert-small-finetuned-squadv2"
 
 tokenizer = AutoTokenizer.from_pretrained(name,)
@@ -68,6 +70,9 @@ def index():
 
       return render_template("index.html",result = result)
 
+    # answer = answer_question(question, bert_abstract)
+
+    # return answer
     return render_template("index.html")
 
 if __name__ == '__main__':
